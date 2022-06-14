6e25a52bc10e8f050fe2cd96e8e12c50c0719883
he2so0 <heesoo9913@gmail.com>
2022-06-14 18:31:19 +0900

    feat: 6/14, model, repository 생성

--- backend/src/main/java/com/team2/backend/domain/employee/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team2.backend.domain.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-    Employee findByUserId(String userId);
-    Boolean existsByUserId(String userId);
-}
