9fca78a33db8484f6ce719a5efbc90b3f032db44
he2so0 <heesoo9913@gmail.com>
2022-06-15 09:35:02 +0900

    feat: 6/15, admin header, sidebar pages 변경

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.js";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
