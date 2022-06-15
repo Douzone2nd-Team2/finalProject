915715cc3f53e97618c244aad4e78718c8643de2
chaerin99 <cofls990111@naver.com>
2022-06-15 16:33:19 +0900

    feat: 관리자 header, sidebar 변경중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./admin/components/Share/Header";
 import Sidebar from "./admin/components/Share/Sidebar";
+// import Resource from "./admin/components/Resource/ResourcePage";
 
 function App() {
   return (
@@ -7,18 +8,7 @@ function App() {
       <header className="App-header">
         <Header />
         <Sidebar />
-
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        {/* <Resource /> */}
       </header>
     </div>
   );
