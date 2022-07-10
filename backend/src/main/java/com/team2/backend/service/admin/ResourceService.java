b8ad288fe08e588352ba610be92d00399f03b46a
he2so0 <heesoo9913@gmail.com>
2022-07-11 02:00:01 +0900

    fix: 7/11, resourceController parameter fix

--- backend/src/main/java/com/team2/backend/service/admin/ResourceService.java
+++ backend/src/main/java/com/team2/backend/service/admin/ResourceService.java
@@ -273,7 +273,7 @@ public class ResourceService {
     }
 
     @Transactional
-    public ResponseEntity<Message> fileUpdate(List<MultipartFile> multipartFile, Long resourceNo) {
+    public ResponseEntity<Message> fileUpdate(List<MultipartFile> multipartFile) {
         try {
             List<Resourcefile> resourcefileList = new ArrayList<>();
 
@@ -402,29 +402,5 @@ public class ResourceService {
                 .data(list)
                 .build();
         return new JsonResponse().send(200, message);
-    }
-
-    @Transactional
-    public ResponseEntity<Message> searchResource(String keyword){
-        Message message;
-        try {
-            List<ResourceDto> resourceList = reservationQuerydslRepository.getSearchResourceList(keyword);
-
-            message = Message.builder()
-                    .resCode(1000)
-                    .message("[Success] : Select resourceSearchList")
-                    .data(resourceList)
-                    .build();
-            return new JsonResponse().send(200, message);
-
-        }catch (Exception e){
-            e.printStackTrace();
-            message = Message.builder()
-                    .resCode(1001)
-                    .message("[Fail] : Select resourceSearchList")
-                    .build();
-            return new JsonResponse().send(400, message);
-        }
-
     }
 }
