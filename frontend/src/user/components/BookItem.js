9fb9b0748a586ab31f509b3685d5b3e924daf6eb
Lee <minmin1243@naver.com>
2022-07-12 20:19:09 +0900

    feat: 7/12 사용자 예약 현황 <-> 예악 정보 연동

--- frontend/src/user/components/BookItem.js
+++ frontend/src/user/components/BookItem.js
@@ -1,13 +1,55 @@
+import { useNavigate } from 'react-router-dom';
+
+import axios from 'axios';
+
+import { getCookie } from '../utils/cookie';
+
 import BookContainer from '../styles/BookItem';
 
 const BookItem = ({ book }) => {
-  const { resourceName, startTime, endTime, imageUrl, reservName } = book;
+  const { resourceName, startTime, endTime, imageUrl, reservName, reservNo } =
+    book;
 
   const [startDate, startT] = startTime.split('T');
   const [endDate, endT] = endTime.split('T');
 
+  const navigate = useNavigate();
+
+  const clickBtn = (e, reservNo) => {
+    e.preventDefault();
+    axiosGetMyReservationInfo(reservNo);
+  };
+
+  const axiosGetMyReservationInfo = async (reservNo) => {
+    console.log(reservNo);
+    const myReservationInfo = await axios
+      .post(
+        `${process.env.REACT_APP_SERVER_PORT}/mypage/getMyReservationInfo`,
+        reservNo,
+        {
+          headers: {
+            Authorization: getCookie('accessToken'),
+            'Content-type': 'application/json',
+          },
+        },
+      )
+      .then((res) => {
+        if (res.data.resCode === 4001) {
+          alert('[ERROR] 알 수 없는 오류가 발생하였습니다.');
+          console.log(res.data);
+          return;
+        } else {
+          navigate('/mypage/reservation', {
+            state: res.data.data,
+          });
+          return res.data.data;
+        }
+      })
+      .catch(console.error);
+  };
+
   return (
-    <BookContainer>
+    <BookContainer onClick={(e) => clickBtn(e, book.reservNo)}>
       {imageUrl && (
         <div className="thumbnail">
           <img src={imageUrl} alt="No Image" />
